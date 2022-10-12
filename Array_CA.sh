d1799e9bb00c9eddb8687e6cd37e6cfd7c7f55d2
edinaan <21083010022@student.upnjatim.ac.id>
2022-10-12 17:06:57 +0700

    Tugas 6

---
+++ Array_CA.sh
@@ -0,0 +1,8 @@
+# deklarasi array compound assignment
+distroLinuxDesktop=('BlankOn' 'Ubuntu' 'Debian' 'ArchLinux' 'LinuxMint')
+distroLinuxServer=('UbuntuServer' 'CentOS' 'FedoraServer')
+
+# cara mengambil nilai array
+echo ${distroLinuxDesktop[*]}
+echo ${distroLinuxServer[*]}
+
